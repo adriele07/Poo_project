36a766ec712642cd58524369fa64e7647390ba94
Adriele <118401626+adriele07@users.noreply.github.com>
2025-06-19 18:46:29 -0300

    onde adicionei a feiura

--- projeto/front-end/src/App.jsx
+++ projeto/front-end/src/App.jsx
@@ -4,17 +4,28 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 axios.defaults.baseURL = import.meta.env.VITE_AXIOS_BASE_URL;
 
 function App() {
   const [user, setUser] = useState(null);
+  //  useEffect para restaurar o usuário do localStorage ao iniciar o app.
+  // Isso garante que o usuário continue logado mesmo após atualizar a página (F5).
+  useEffect(() => {
+    const savedUser = localStorage.getItem("user");
+    if (savedUser) {
+      setUser(JSON.parse(savedUser));
+    }
+  }, []);
 
   return (
     <div className="flex h-screen flex-col overflow-hidden">
       <BrowserRouter>
-        <Header user = {user} />
+              {/* [A feiura que eu disse que adicionei Cayo] Agora o Header recebe setUser para permitir logout ao clicar em "Sair" */}
+        <Header user={user} setUser={setUser} />
+
 
         {/* Configurando rotas para deixar o site mais dinâmico */}
         <main className="no-scrollbar flex-1 overflow-y-auto">
