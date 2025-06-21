0a1611ef3c10e1e2ad1437258bcd10607c96b963
Adriele <118401626+adriele07@users.noreply.github.com>
2025-06-21 17:14:38 -0300

    Delete hashbnb-youtube-main directory

--- hashbnb-youtube-main/back-end/index.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { app } from "./server.js";
-
-const { PORT } = process.env;
-
-app.listen(PORT, () => {
-  console.log(`Servidor está rodando na porta ${PORT}`);
-});
